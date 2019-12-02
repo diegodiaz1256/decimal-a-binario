3272e98574133234a14d11e69d59ba994be14405
diegodiaz1256 <fernandezdiego3758@gmail.com>
2019-12-02 09:07:16 +0000

    codigo para pasar a decimal de binario

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <string.h>
 #include <math.h>
 #include <stdio.h>
-void binario (int num);
 int decimal (char * a, int len);
 int main(void) {
   char array[50];
@@ -17,22 +16,10 @@ int decimal(char * a,int len){
     return 0;
   }
   else if('0'!=*a){
-    printf("%c\n",*a);
     a++;
     return pow(2,len-1)+decimal(a,len-1);
   }else if('0'==*a){
     a++;
     return decimal(a,len-1);
   }
-}
-void binario(int num){
-   int aux;
-   
-  if(num==0){
-    return;
-  }
-  aux=num%2;
-  num=num/2;
-  binario(num);
-  printf("%d\n",aux);
 }
